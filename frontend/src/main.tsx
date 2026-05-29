ee029283e69ceea1b0ef29f14684453fe5345c25
Lihuen Segovia Grabois <lihuensg@gmail.com>
2026-05-29 11:35:21 -0300

    feat: agregar componente DisablePinchZoom para prevenir el zoom por gestos en dispositivos táctiles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,41 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useEffect } from 'react'
 import 'flag-icons/css/flag-icons.min.css'
 import './index.css'
 import App from './App.tsx'
 
+function DisablePinchZoom() {
+  useEffect(() => {
+    const preventGestureZoom = (event: Event) => {
+      event.preventDefault()
+    }
+
+    const preventPinchZoom = (event: TouchEvent) => {
+      if (event.touches.length > 1) {
+        event.preventDefault()
+      }
+    }
+
+    document.addEventListener('gesturestart', preventGestureZoom)
+    document.addEventListener('gesturechange', preventGestureZoom)
+    document.addEventListener('gestureend', preventGestureZoom)
+    document.addEventListener('touchmove', preventPinchZoom, { passive: false })
+
+    return () => {
+      document.removeEventListener('gesturestart', preventGestureZoom)
+      document.removeEventListener('gesturechange', preventGestureZoom)
+      document.removeEventListener('gestureend', preventGestureZoom)
+      document.removeEventListener('touchmove', preventPinchZoom)
+    }
+  }, [])
+
+  return null
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <DisablePinchZoom />
     <App />
   </StrictMode>,
 )
